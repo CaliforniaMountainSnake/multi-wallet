0b6f7eef385df68e77e911db87ed439f5e055518
CaliforniaMountainSnake <CaliforniaMountainSnake1@yandex.ru>
2022-05-02 18:39:41 +0300

    Bundle size was reduced to 653 kb using "purgecss" lib. Webpack configuration was splitted into multiple files.

---
+++ webpack.common.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.tsx",
+    output: {
+        path: path.resolve(__dirname, "./www"),
+        filename: "bundle.js",
+        clean: true,
+    },
+    resolve: {
+        extensions: [".js", ".jsx", ".ts", ".tsx", "..."],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?jsx?$/,
+                exclude: /node_modules/,
+                use: {loader: "babel-loader"}
+            },
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                use: {loader: "ts-loader"}
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader", "postcss-loader"],
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin(),
+    ],
+};
